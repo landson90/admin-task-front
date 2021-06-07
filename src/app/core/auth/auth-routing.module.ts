936dc95f554337247a6ee13dc63a167b472412f3
landson <randelsilva1990@gmail.com>
2021-06-07 20:07:35 -0300

    criando o layout da tela do de login .

---
+++ src/app/core/auth/auth-routing.module.ts
@@ -0,0 +1,23 @@
+import { SingComponent } from './sing/sing.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        component: SingComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
